01cc2022583ab2af413eb3ec0fc300472808c8eb
vdloc <uvcudlco@gmail.com>
2022-09-22 16:19:47 +0700

    remove redundant locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    locales: ['en', 'de', 'es', 'ar', 'he', 'zh'],
+    locales: ['en'],
     defaultLocale: 'en',
     // localeDetection: false,
   },
